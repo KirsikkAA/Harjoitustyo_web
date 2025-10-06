ba6684b3d10846c7a7f42c766eb7ce4ac30d361a
KirsikkAA <kirsikka828@gmail.com>
2025-10-06 15:40:22 +0300

    first commit

---
+++ easy.js
@@ -0,0 +1,113 @@
+const questions = [
+    {
+        question: "Who was the first Finnish champion?/HELPPO",
+        answers: [
+            { text: "Kimi Räikkönen", correct: false},
+            { text: "Keke Rosberg", correct: true},
+            { text: "Nico Rosberg", correct: false},
+            { text: "Mika Häkkinen", correct: false},
+        ]
+    },
+    {
+        question: "Which team Lewis Hamilton has not competed/HELPPO",
+        answers: [
+            { text: "Mercedes", correct: false},
+            { text: "Mclaren", correct: false},
+            { text: "Ferrari", correct: false},
+            { text: "Williams", correct: true},
+        ]
+
+    }
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answerBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentQustionIndex = 0;
+let score = 0;
+
+
+function startQuiz(){
+    currentQustionIndex = 0;
+    score = 0;
+    nextBtn.innerHTML = "Seuraava";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQustionIndex];
+    let questionNo = currentQustionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function  resetState(){
+    nextBtn.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextBtn.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `Sait ${score}/${questions.length} oikein!`;
+    nextBtn.innerHTML = "Pelaa uudestaan";
+    nextBtn.style.display = "block";
+    backBtn.style.display = "block";
+}
+
+
+function handleNextquestion(){
+    currentQustionIndex++;
+    if(currentQustionIndex < questions.length){
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextBtn.addEventListener("click", () => {
+    if(currentQustionIndex < questions.length){
+        handleNextquestion();
+    } else {
+        startQuiz();
+    }
+});
+
+backBtn.addEventListener("click", () => {
+    window.location.href = 'pelaa.html';
+});
+
+startQuiz();
